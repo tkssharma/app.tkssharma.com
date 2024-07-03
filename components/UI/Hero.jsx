09017a5a6f1126432c4080d43bb02d2889dfab10
tkssharma <tarun.softengg@gmail.com>
2024-07-03 19:26:37 +0530

    blogs

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
                 <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r animate-pulse hover:animate-none from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
                 <Link
                   target="_blank"
-                  href="https://discord.gg/tkssharma"
+                  href="https://discord.gg/ZbxMJBGx"
                   className="relative text-sm sm:text-md md:text-lg text-center items-center justify-center px-8 py-4  font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   role="button"
                 >
